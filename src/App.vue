13deda3540e135feb2527d947af368e9a52adca1
lizi <764793764@qq.com>
2020-12-25 01:31:10 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Count
+      v-for="source in sources"
+      :key="source.number"
+      v-bind:value="source"
+    />
+    <div class="sources">
+      <div v-for="item in sources" :key="item.name">
+        {{ item.name }}:{{ item.count }}
+      </div>
+    </div>
+    <div class="numbers">
+      <div v-for="item in numberCountItem" :key="item.name">
+        {{ item }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Count from "./components/Count.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Count
+  },
+  data() {
+    return {
+      sources: [
+        {
+          number: "N034",
+          name: "男士",
+          price: 15,
+          count: 0
+        },
+        {
+          number: "V034",
+          name: "女士",
+          price: 15,
+          count: 0
+        },
+        {
+          number: "E034",
+          name: "儿童",
+          price: 7,
+          count: 0
+        }
+      ]
+    };
+  },
+  computed: {
+    numberCountItem() {
+      const numbers = this.sources.map(v => {
+        let list = [];
+        let item = v.number;
+        for (let i = 0; i <= v.count; i++) {
+          item = this.format(item);
+          list.push(item);
+        }
+        return list;
+      });
+      return [].concat.apply([], numbers);
+    }
+  },
+  methods: {
+    format(item) {
+      const count = item.match(/(\d+)([^\d]*)$/i)[1];
+      return item.replace(
+        /(\d+)([^\d]*)$/i,
+        this.PrefixInteger(Number(count) + 1, count.length) + "$2"
+      );
+    },
+    PrefixInteger(num, n) {
+      return (Array(n).join(0) + num).slice(-n);
+    }
   }
 };
 </script>
@@ -24,5 +87,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 500px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+.sources {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+}
+.numbers > div {
+  float: left;
+  padding: 4px 0;
+}
+.numbers > div::after {
+  content: "、";
+}
+.numbers > div:last-child::after {
+  display: none;
 }
 </style>
---
+++ src/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="count-component">
+    <div class="name">{{ value.name }}</div>
+    <div class="price">￥{{ value.price }}</div>
+    <div class="count">
+      <button @click="action('reduce')">-</button>
+      <input type="text" v-model="value.count" />
+      <button @click="action('addition')">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    action(type) {
+      if (type === "reduce") {
+        // count大于0才可以减
+        if (this.value.count > 0) {
+          this.value.count--;
+        }
+      } else {
+        this.value.count++;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.count-component {
+  display: flex;
+  flex-flow: row;
+  width: 100%;
+  justify-content: space-between;
+}
+.name {
+  width: 100px;
+}
+.price {
+  width: 100px;
+}
+</style>
